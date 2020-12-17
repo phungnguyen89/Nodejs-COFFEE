468059835c9b470990016f245b6635d7b36095fd
phungnguyen89 <ntphung18@apcs.vn>
2020-12-17 11:55:28 +0700

    update frontend productinfo

--- src/test/array.js
+++ src/test/array.js
@@ -1,8 +1,16 @@
-let a = [1, 2, 1, 3, 4, 5];
-// console.log(a.splice(a.indexOf(2), 1), a);
-//console.log(a.splice(0, 2), a);
-// let b = Array.from(a);
-// b.push(10);
-// console.log(a);
-// console.log(b);
-// a.splice(0, 1);
+// let a = [1, 2, 1, 3, 4, 5];
+let a = [
+  {
+    id: 1,
+    name: "n1",
+  },
+  {
+    id: 2,
+    name: "n2",
+  },
+  {
+    id: 3,
+    name: "n3",
+  },
+];
+console.log(a.indexOf({ name: "n1" }));
